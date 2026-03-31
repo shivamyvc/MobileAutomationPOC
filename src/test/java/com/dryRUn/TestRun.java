5d3bc98058c9b3b9114eddd7f7c369fcb73285b7
Shivam <shivamyvc@gmail.com>
2026-03-31 10:05:18 +0530

    Vehicle Selection Flow Implemented

--- src/test/java/com/dryRUn/TestRun.java
+++ src/test/java/com/dryRUn/TestRun.java
@@ -9,24 +9,31 @@ import ui.pages.MapScreenPage;
 
 public class TestRun extends BaseTest{
 
-	@Test(description = "Select Location")
+	@Test(description = "Select Location",enabled = false)
 	public void testRun1() {
 		MapScreenPage MapHome= new MapScreenPage();
 		 MapHome.goToSearch("New York").select("New York");
 	}
 	
 	
-	@Test(description = "Add Vehicle & adaptor details")
+	@Test(description = "Add Vehicle & adaptor details",enabled = false)
 	public void testRun2() {
 		MapScreenPage MapHome= new MapScreenPage();
 		MapHome.goToAccount().vehicle().makeAndModel("Tesla","Model 3").adapter("CCS").adapter("J-1772");
 	}
 	
 	
-	@Test(description = "Add payment method details")
+	@Test(description = "Add payment method details",enabled = false)
 	public void testRun3() {
 		MapScreenPage MapHome= new MapScreenPage();
 		MapHome.goToAccount().payment();
+	}
+	
+	
+	@Test(description = "Scroll and select vehichle details")
+	public void testRun4() {
+		MapScreenPage MapHome= new MapScreenPage();
+		MapHome.goToAccount().vehicle().makeAndModel("Volkswagen", "ID.4").adapter("CHAdeMO").saveVehicleDetails();
 	}
 	
 
